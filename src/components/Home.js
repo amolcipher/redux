39b6f0e755905c899c0a0cd3c9526d88c2be463a
amol <amolchaudhari1305@gmail.com>
2022-09-20 15:17:23 +0530

    add to card design done

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+const Home=()=>{
+    return(
+        <>
+            <h1>Home</h1>
+            <div className='main'>
+                <div>
+
+                    <img src="../../images/mob1.jpg"></img>
+                </div>
+                <div>price :200</div>
+                <div><button >submit</button></div>
+
+            </div>
+        </>
+    )
+
+}
+export default Home;
